62557463a071f9a2bc1855101183753a5f8c618b
Satendra Singh <satendra123@gmail.com>
2026-06-08 18:33:44 +0530

    CharacterArray

---
+++ src/Strings/CharacterArray.java
@@ -0,0 +1,13 @@
+package Strings;
+public class CharacterArray {
+    public static void main(String[] args) {
+        char[] arr = {'m', 'u', 'm', 'm', 'y'};
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        for (char ch : arr) {
+            System.out.print((char) ch + " ");
+        }
+    }
+}
